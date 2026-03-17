c378ad2a3c2f6ff976810e479382374da74b9612
shBLOCK <shblock3@gmail.com>
2026-03-16 20:33:21 -0700

    Fix block destruction progress rendering

--- src/main/java/dev/shblock/glassfree3d/mixin/LevelRendererMixin.java
+++ src/main/java/dev/shblock/glassfree3d/mixin/LevelRendererMixin.java
@@ -10,7 +10,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -57,5 +59,10 @@ public abstract class LevelRendererMixin implements LevelRendererAccessor {
         if (frustum instanceof NoCullFrustum) {
             cir.setReturnValue(frustum);
         }
+    }
+    
+    @ModifyConstant(method = "renderLevel", constant = @Constant(doubleValue = 1024.0F), allow = 1)
+    private double modifyBlockDestructionProgressRenderRange(double _value) {
+        return Float.POSITIVE_INFINITY;
     }
 }
