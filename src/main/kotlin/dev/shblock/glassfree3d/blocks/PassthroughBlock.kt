41665bb68a97b0b7a96ac92da79fc18d987c5075
shBLOCK <shblock3@gmail.com>
2026-03-17 17:31:37 -0700

    Passthrough block no collision

--- src/main/kotlin/dev/shblock/glassfree3d/blocks/PassthroughBlock.kt
+++ src/main/kotlin/dev/shblock/glassfree3d/blocks/PassthroughBlock.kt
@@ -2,4 +2,4 @@ package dev.shblock.glassfree3d.blocks
 
 import net.minecraft.world.level.block.Block
 
-class PassthroughBlock : Block(Properties.of().strength(1.5f, 6f))
+class PassthroughBlock : Block(Properties.of().strength(1.5f, 6f).noCollission())
